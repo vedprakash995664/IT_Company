013556670a707846474b0e38ed611c18ce4784a1
Ved Prakash <vedprakash995664@gmail.com>
2025-05-03 17:46:07 +0530

    Done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -38,7 +38,7 @@ function Header() {
               <a href="https://facebook.com" target="_blank" rel="noreferrer"><FaFacebookF /></a>
               <a href="https://twitter.com" target="_blank" rel="noreferrer"><FaTwitter /></a>
               <a href="https://instagram.com" target="_blank" rel="noreferrer"><FaInstagram /></a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+              <a href="https://www.linkedin.com/in/ved-prakash-93436518a/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
             </div>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
